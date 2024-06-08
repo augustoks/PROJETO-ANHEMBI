1150ed046712703939a610f4f2a2e7688be00b17
Augusto Paiva <150201005+augustoks@users.noreply.github.com>
2024-06-08 20:17:48 -0300

    script.js

--- script.js
+++ script.js
@@ -1,17 +1,42 @@
+// o elemento que representa o Mario
 const mario = document.querySelector('.mario');
+
+// elemento que representa o tubo
 const pipe = document.querySelector('.pipe');
 
-const jump = () => {
-  mario.classList.add('jump');
-  setTimeout(() => {
-    mario.classList.remove('jump');
-  }, 500);
+// altura do pulo
+const jumpHeight = 150;
+
+// vai definir a velocidade do pulo
+const jumpSpeed = 370 ;
+
+// Variável que indica se o Mario está pulando
+let isJumping = false;
+
+// Função que vai fazer o Mario pular
+function jump() {
+  if (!isJumping) {
+    isJumping = true;
+    // Anima o Mario para cima
+    mario.style.transform = `translateY(-${jumpHeight}px)`;
+    mario.style.transition = `transform ${jumpSpeed}ms`;
+
+    // Aguarda o fim da animação
+    setTimeout(() => {
+      // Anima o Mario para baixo
+      mario.style.transform = `translateY(0px)`;
+      mario.style.transition = `transform ${jumpSpeed}ms`;
+      isJumping = false;
+    }, jumpSpeed);
+  }
 }
 
-c
-
-
-
+// Adiciona um evento de teclado para detectar a tecla espaço
+document.addEventListener('keydown', (event) => {
+  if (event.key === ' ') {
+    jump();
+  }
+});
 
 
 
